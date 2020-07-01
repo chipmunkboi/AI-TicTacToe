7ed04d905f24924b918785d5564a58a05dd38e46
Jeffrey Zhang <jeffreyzhang2@gmail.com>
2020-07-01 00:32:01 -0700

    Initial commit

---
+++ mcts_vanilla.py
@@ -0,0 +1,103 @@
+
+from mcts_node import MCTSNode
+from random import choice
+from math import sqrt, log
+
+num_nodes = 1000
+explore_faction = 2.
+
+def traverse_nodes(node, board, state, identity):
+    """ Traverses the tree until the end criterion are met.
+
+    Args:
+        node:       A tree node from which the search is traversing.
+        board:      The game setup.
+        state:      The state of the game.
+        identity:   The bot's identity, either 'red' or 'blue'.
+
+    Returns:        A node from which the next stage of the search can proceed.
+
+    """
+    # Uses upper confidence bound to either exploit or explore new nodes
+    # Traverse down tree with biasing choice for child nodes until leaf node and return
+
+
+
+    
+    pass
+    # Hint: return leaf_node
+
+
+def expand_leaf(node, board, state):
+    """ Adds a new leaf to the tree by creating a new child node for the given node.
+
+    Args:
+        node:   The node for which a child will be added.
+        board:  The game setup.
+        state:  The state of the game.
+
+    Returns:    The added child node.
+
+    """
+
+    # Create new node (random state) and return the node
+
+
+    pass
+    # Hint: return new_node
+
+
+def rollout(board, state):
+    """ Given the state of the game, the rollout plays out the remainder randomly.
+
+    Args:
+        board:  The game setup.
+        state:  The state of the game.
+
+    """
+
+    # From random state play random game and record win rate?
+    # Play k random games and record the win rate to visit rate of the new node?
+
+    pass
+
+
+def backpropagate(node, won):
+    """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
+
+    Args:
+        node:   A leaf node.
+        won:    An indicator of whether the bot won or lost the game.
+
+    """
+
+    # Go back up to the root node (starting state) while updating win/visit values of every node along path
+
+    pass
+
+
+def think(board, state):
+    """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
+
+    Args:
+        board:  The game setup.
+        state:  The state of the game.
+
+    Returns:    The action to be taken.
+
+    """
+    identity_of_bot = board.current_player(state)
+    root_node = MCTSNode(parent=None, parent_action=None, action_list=board.legal_actions(state))
+
+    for step in range(num_nodes):
+        # Copy the game for sampling a playthrough
+        sampled_game = state
+
+        # Start at root
+        node = root_node
+
+        # Do MCTS - This is all you!
+
+    # Return an action, typically the most frequently used action (from the root) or the action with the best
+    # estimated win rate.
+    return None
